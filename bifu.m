14baf4afca35e5f1d68e379ae0a304fa89ae3af5
nguyentrong <nguyentrongmanh@yahoo.com>
2013-04-28 11:44:45 +0200

    Update bifu.m Use constant initial conditions insteed of random Reduce the map resolution to 9x6 07.05.13:  1-Use 5 weighting factor f1 f2 f3 f4 f5 for 5 types of connectivity  2-Use original values of a and b for connectivity formula: w 0 a*exp(-b*dphi)

--- bifu.m
+++ bifu.m
@@ -1,102 +1,103 @@
-<<<<<<< HEAD:bifu.m
-function bifu(p_filename,res_filename,se,si,a1,a2)
-=======
-function bifu(p_filename,res_filename,a1,a2)
->>>>>>> 420fe672a837244b6d6ca9dea3d07a3a69cb9503:bifu.m
+function bifu(p_filename,res_filename)
 % Version infomation:
 % 1 - Weighting the matrix in the connectivity function
 % 2 - Wichten the selectivity smax unterschiedlich fuer excitatory und inhibitory
-
+% 3 - 09.05.13 Change structure of p 
 
     %% close and clear all
     close all; 
     warning off;  
     addpath('input');
-    
-    %disp('Starting...');
-    fprintf('%s\t%s\t%.5e\t%.5e\t%.5e\t%.5e\n',p_filename,res_filename,se,si,a1,a2);
-      
+         
     %% Define global variables
-    global  x y nmass te ti he hi semax simax e0 r0 v0 n sz neqn tstart tend ...
-        p path signal_cut T
+    global  x y nmass te ti he hi e0 r0 v0 n sz neqn tend ...
+        c1s c1n c1l c2s c2n c2l f1 f2 f3 f4 f5 p sample_factor path 
      
     %% Load initial data file  
     % te=p(1); ti=p(2); he=p(3); hi=p(4); 
-    % smax=p(5); d=p(6); x=p(7); y= p(8); sz = p(9);
-    % c1s=p(10); c1n=p(11); c1l=p(12);
-    % c2s=p(13); c1n=p(14); c1l=p(15);
+    % sz = p(5); sample_factor = p(6)
+    % c1s=p(7); c1n=p(8); c1l=p(9);
+    % c2s=p(10); c2n=p(11); c2l=p(12);
+    % f1 = p(13); f2 = p(14); f3 = p(15);
+    % f4 = p(16); f5 = p(17); tend = p(18);
     % disp('Loading initial parameter...');
     %load(['param/' filename '.mat']);
     load([p_filename '.mat']);
-    %disp(['# te=' num2str(p(1)) 'ms; ti=' num2str(p(2)) 'ms; he=' num2str(p(3)) 'mV; hi=' num2str(p(4)) 'mV;']);
-    %disp(['# semax=' num2str(p(5)) '; simax=' num2str(p(6)) '; x=' num2str(p(7)) '; y=' num2str(p(8)) '; sz=' num2str(p(9)) ]);
-    %disp(['# c1s=' num2str(p(10)) '; c1n=' num2str(p(11),'%.3f') '; c1l=' num2str(p(12),'%.3f')]);
-    %disp(['# c2s=' num2str(p(13)) '; c2n=' num2str(p(14),'%.3f') '; c2l=' num2str(p(15),'%.3f')]);
-    %disp(['# tstart=' num2str(p(16)) '; tend=' num2str(p(17))]);
-       
+    fprintf('[%.3f %.3f %.3f %.3f %.3f %.3f] %s %s %.4f %.4f %.4f %.4f %.4f\n',...
+        p(7),p(8),p(9),p(10),p(11),p(12),p_filename,res_filename,p(13),p(14),p(15),p(16),p(17));     
+    
     %% Parameters
     % synaptic and dendrite parameter
-    te       = p(1);% 10  % ms
-    ti       = p(2);% 20; % ms
-    he       = p(3);% 3.25;  % mV
-    hi       = p(4);% -22;% mV
-    semax    = se;%p(5);% 10;
-    simax    = si;%p(6);
+    te      = p(1);% 10  % ms
+    ti      = p(2);% 20; % ms
+    he      = p(3);% 3.25;  % mV
+    hi      = p(4);% -22;% mV
+    sz      = p(5);
+    c1s     = p(7);
+    c1n     = p(8);
+    c1l     = p(9);
+    c2s     = p(10);
+    c2n     = p(11);
+    c2l     = p(12);
+    f1      = p(13);
+    f2      = p(14);
+    f3      = p(15);
+    f4      = p(16);
+    f5      = p(17);
+    tend    = p(18);
+    sample_factor = p(6);
     % parameter of sigmoid function
     e0       = 2.5e-3;% 1/ms
     r0       = 0.56;  % 1/mV
     v0       = 6;     % mV
     % others
-    x        = p(7);% 31 field size
-    y        = p(8);% 21 field size
+    x        = sample_factor*90;% 31 field size
+    y        = sample_factor*60;% 21 field size
     n        = x*y;
     nmass    = n*3*8;
     neqn     = 2*nmass;
-    sz       = p(9); % 0.1; % ms
-    % integration time
-    tstart   = p(16);   % ms
-    tend     = p(17);   % ms
-    signal_cut = 1;
-    T        = [];
        
     % Result path
     path     = 'output';
 
     %% Solving the ode system
-    [OM] = VC_ode(a1,a2);
-
+    [OM] = VC_ode();
+    fname = [path '/c1s=' num2str(p(10)) '_c1n=' num2str(p(11)) '_c1l=' num2str(p(12))...
+        '_c2s=' num2str(p(13)) '_c2n=' num2str(p(14)) '_c2l=' num2str(p(15)) '_f1='...
+        num2str(f1) '_f2=' num2str(f2) '_f3=' num2str(f3) '_f4=' num2str(f4) '_f5=' num2str(f5)];
+    %save([fname '.mat'],'OM');
+    plotLenin(fname,OM);  
+    
     OM = OM(:,end-200:end);
-    OM = [ sum(OM(1:n,:));
-     sum(OM(n+1:2*n,:));
-     sum(OM(2*n+1:3*n,:));
-     sum(OM(3*n+1:4*n,:));
-     sum(OM(4*n+1:5*n,:));
-     sum(OM(5*n+1:6*n,:));
-     sum(OM(6*n+1:7*n,:));
-     sum(OM(7*n+1:8*n,:))  ];
-    OM = OM/n;
+    OM = [ sum(OM(1:n,:));       sum(OM(n+1:2*n,:)); sum(OM(2*n+1:3*n,:)); ...
+           sum(OM(3*n+1:4*n,:)); sum(OM(4*n+1:5*n,:)); sum(OM(5*n+1:6*n,:)); ...
+           sum(OM(6*n+1:7*n,:)); sum(OM(7*n+1:8*n,:)) ];
+
     minOM = min(OM,[],2);
     maxOM = max(OM,[],2);
-    res = [simax semax a1 a2 minOM(:)' maxOM(:)'];  
+    res = [f1 f2 f3 f4 f5 minOM(:)' maxOM(:)'];  
     
     fid = fopen([res_filename '.txt'],'a+');    
     for i = 1:length(res)
-        fprintf(fid,'%.8f\t',res(i)); 
+        fprintf(fid,'%.6f\t',res(i)); 
     end
     fprintf(fid,'\n');
-    fclose(fid);    
+    fclose(fid);
+    
+    for i = 6:8; fprintf('%.9f\t',res(i)); end;  fprintf('\n');
+    for i = 14:16; fprintf('%.9f\t',res(i)); end; fprintf('\n');
+  
+    clear, clearvars -global
     %disp('Finished!');
 end
 
-function [OM] = VC_ode(a1,a2)
+function [OM] = VC_ode()
 %% ODE solver using Euler method with constant stepsize 
-
-global te ti he hi e0 r0 v0 nmass tstart sz tend signal_cut
+global te ti he hi e0 r0 v0 nmass sz tend
 
 % The connectivity and the weighting matrix
 %disp('Loading the connectivity matrix...');
-[W] = connectivity(a1,a2); 
-%clear wtM
+[W] = connectivity(); 
     
 % modify the connectivity
 ke = he/te; ki = hi/ti;
@@ -112,7 +113,9 @@ n_trunk = ceil(tend/trunk/sz);
 %%
 % wtM = ones(nmass,1);
 OM = [];  % Orientation map
-Y0 = [rand(nmass,1); rand(nmass,1)];% Initial condition
+%Y0 = [zeros(nmass,1); zeros(nmass,1)];% Initial condition
+%Y0(1) = -70; Y0(nmass+1) = -70;
+Y0 = rand(2*nmass,1);
 dv = zeros(nmass,1);
 du = dv;
 
@@ -120,11 +123,10 @@ du = dv;
 
 % additional info
 trunkidx = 1; 
-tic;
 
-%fprintf('[%.2f %.2f %.2f %.2f](%d):\n',te,ti,he,hi,n_trunk);
+fprintf('Processing (total %d):    \t',n_trunk);
 
-for idx = 1:length(tstart:sz:tend)      
+for idx = 1:length(0:sz:tend)      
     % Update half of Y0   
     u = Y0(1:nmass); 
     v = Y0(nmass+1:2*nmass);
@@ -151,39 +153,34 @@ for idx = 1:length(tstart:sz:tend)
     % Save result in each step   
     OM = [OM u(1:end/3)+u(end/3+1:2*end/3)+u(2*end/3+1:end)];
       
-    %if ~mod(idx,trunk)
+    if ~mod(idx,trunk)
        % display the informations
-       %estTime = toc;
-       %fprintf('%.3d(%.1fs) ',trunkidx,estTime);
-       %if ~mod(trunkidx,5); fprintf('\n'); end;
-       %trunkidx = trunkidx + 1;
-       %tic;
-    %end
+       fprintf('\b\b\b\b%04d',trunkidx);
+       trunkidx = trunkidx + 1;
+    end
 
 end
 
 %% End of solver loops
-    
+fprintf('\n');    
 end
 
 
-function [W] = connectivity(a1,a2)
+function [W] = connectivity()
 % Changed 23.04.13: Die Winkeldifferenz zwischen 157.5 und 0 is 22.5 
 % (nicht 157.5 wie vorher)
 
-    global x y p semax simax
+    global x y f1 f2 f3 f4 f5 c1s c1n c1l c2s c2n c2l sample_factor 
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% %%%%%%%%%%%%%%%%%% Weighting Matrix
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %disp('# the weighting matrix');  
-    c1s = p(10); c1n = p(11); c1l = p(12);
-    c2s = p(13); c2n = p(14); c2l = p(15);   
+    %disp('# the weighting matrix');   
     % Pyramidal and excitatory  
-    tmp1 = [repmat(c1s,x*y,x*y) repmat(c1n,x*y,x*y) repmat(c1l,x*y,6*x*y)];
+    tmp1 = [repmat(c1s,x*y,x*y) repmat(c1l,x*y,6*x*y) repmat(c1n,x*y,x*y)];
     wtMp = tmp1;
     % Inhibitory  
-    tmp2 = [repmat(c2s,x*y,x*y) repmat(c2n,x*y,x*y) repmat(c2l,x*y,6*x*y)];
+    tmp2 = [repmat(c2s,x*y,x*y) repmat(c2l,x*y,6*x*y) repmat(c2n,x*y,x*y)];
     wtMi = tmp2;
     for i = 1:7
         wtMp = [wtMp; circshift(tmp1,[0 i*x*y])];
@@ -201,9 +198,6 @@ function [W] = connectivity(a1,a2)
     d_angle   = 22.5;
     angle     = angle_min:d_angle:angle_max-d_angle;
 
-    % Sample factor
-    sample_factor = 1/5;
-
     %% Load images and estimate the position of pixel
     %disp('CONNFUN::Load images and estimate the position of pixel');
     map = cell(length(angle),1);
@@ -212,13 +206,14 @@ function [W] = connectivity(a1,a2)
         im = im(:,:,1);
         % cut off the images border
         im1 = double(im(11:103,10:72,1));
-        %im1 = im1(1:93,1:63);
+        im1 = im1(2:91,2:61);
         % downsample
         im2 = zeros(size(im1)*sample_factor);
-        z = 0;
         for ridx = 1:size(im2,1)      % row indices 
             for cidx = 1:size(im2,2)  % column indices               
-                im2(ridx,cidx) = im1(1/sample_factor*(ridx-1)+1,1/sample_factor*(cidx-1)+1); 
+                %im2(ridx,cidx) = im1(1/sample_factor*(ridx-1)+1,1/sample_factor*(cidx-1)+1); 
+                im2(ridx,cidx) = sum(sum(im1(1/sample_factor*(ridx-1)+1:1/sample_factor*ridx,...
+                    1/sample_factor*(cidx-1)+1:1/sample_factor*cidx)))/(1/sample_factor*1/sample_factor); 
             end        
         end
         map{ii} = im2;      
@@ -233,8 +228,7 @@ function [W] = connectivity(a1,a2)
     %disp(['CONNFUN::Calculate the selectivity matrix ' num2str(N) ' x ' num2str(N) ' and angle diffenrence']);
     sel = [map{1}(:); map{2}(:); map{3}(:); map{4}(:); map{5}(:); map{6}(:); map{7}(:); map{8}(:)];
     len = length(sel);
-    sel_mat = sel * sel';
-
+    sel_mat = sel * sel'; 
     clear sel
 
     %% Estimate the matrix of orientation differences
@@ -242,7 +236,7 @@ function [W] = connectivity(a1,a2)
     tmp1 = []; for ii = 1:8; tmp1 = [tmp1; repmat(angle(ii),x*y,len)] ; end 
     orientation_difference = abs(tmp1-tmp1');
     orientation_difference(orientation_difference==157.5) = 22.5;
-    orientation_difference(orientation_difference==135) = 45;
+    orientation_difference(orientation_difference==135)   = 45;
     orientation_difference(orientation_difference==112.5) = 67.5;
 
     % TRK: ALternative
@@ -263,7 +257,6 @@ function [W] = connectivity(a1,a2)
         end
     end
 
-
     % Constrain the connectivity 
     %orientation_difference_local(orientation_difference_local>40) = NaN;
     %orientation_difference_distal(orientation_difference_distal>60) = NaN;
@@ -278,7 +271,15 @@ function [W] = connectivity(a1,a2)
     % wie = s_x*s_y*0.08*exp(-0.038*abs(dangle));  Short-range: iN -> P; iN -> eN (>60? -> 0)
     % wii = s_x*s_y*0.04*exp(-0.038*abs(dangle));  Short-range: iN -> iN (>60? -> 0)
     % wex = s_x*s_y*0.01*exp(-0.0072*abs(dangle)); Short-range: P -> P; (>40? -> 0)
-
+    
+    %% 07.05.2013
+    % use 5 weighting factor for 5 types of connectivity
+    % wle = f1*s_x*s_y*0.01*exp(-0.23*abs(dangle));   Long-range: P -> P; P -> eN (>60? -> 0)
+    % wli = f2*s_x*s_y*0.03*exp(-0.023*abs(dangle));  Long-range: P -> iN (>60? -> 0)
+    % wie = f3*s_x*s_y*0.08*exp(-0.038*abs(dangle));  Short-range: iN -> P; iN -> eN (>60? -> 0)
+    % wii = f4*s_x*s_y*0.04*exp(-0.038*abs(dangle));  Short-range: iN -> iN (>60? -> 0)
+    % wex = f5*s_x*s_y*0.01*exp(-0.0072*abs(dangle)); Short-range: P -> P; (>40? -> 0)
+    
     %% Pyramidal
     % P->P = local + distal: 
     diffAngleLocal = orientation_difference_local;
@@ -286,10 +287,8 @@ function [W] = connectivity(a1,a2)
     diffAngleLocal(diffAngleLocal>40) = NaN;
     diffAngleDistal(diffAngleDistal>60) = NaN;
 
-    %Wpp_local = sel_mat*0.01.*exp(-0.0072*diffAngleLocal); Wpp_local(isnan(Wpp_local)) = 0; 
-    %Wpp_distal = sel_mat*0.01.*exp(-0.23*diffAngleDistal); Wpp_distal(isnan(Wpp_distal)) = 0; 
-    Wpp_local = sel_mat/semax*a1.*exp(-0.0072*diffAngleLocal); Wpp_local(isnan(Wpp_local)) = 0; 
-    Wpp_distal = sel_mat/semax*0.01.*exp(-0.23*diffAngleDistal); Wpp_distal(isnan(Wpp_distal)) = 0; 
+    Wpp_local = 1/f5*sel_mat*0.01.*exp(-0.0072*diffAngleLocal); Wpp_local(isnan(Wpp_local)) = 0; 
+    Wpp_distal = 1/f1*sel_mat*0.01.*exp(-0.23*diffAngleDistal); Wpp_distal(isnan(Wpp_distal)) = 0; 
     Wpp = Wpp_distal + Wpp_local;
 
     % P->eN = local + distal
@@ -297,7 +296,7 @@ function [W] = connectivity(a1,a2)
 
     % P->iN = local + distal
     Wpi_local = Wpp_local;
-    Wpi_distal = sel_mat/semax*0.03.*exp(-0.023*diffAngleDistal); Wpi_distal(isnan(Wpi_distal)) = 0; 
+    Wpi_distal = 1/f2*sel_mat*0.03.*exp(-0.023*diffAngleDistal); Wpi_distal(isnan(Wpi_distal)) = 0; 
     Wpi = Wpi_local + Wpi_distal;
 
     % Wp = [Wpp Wpe Wpi];
@@ -320,7 +319,7 @@ function [W] = connectivity(a1,a2)
     diffAngleLocal = orientation_difference_local;
     diffAngleLocal(diffAngleLocal>60) = NaN;
 
-    Wip_local = sel_mat/simax*a2.*exp(-0.038*diffAngleLocal); 
+    Wip_local = 1/f3*sel_mat*0.08.*exp(-0.038*diffAngleLocal); 
     Wip_local(isnan(Wip_local)) = 0; 
     Wip = Wip_local; 
 
@@ -328,7 +327,7 @@ function [W] = connectivity(a1,a2)
     Wie_local = Wip_local;
 
     % iN-iN = local
-    Wii_local = sel_mat/simax*0.04.*exp(-0.038*diffAngleLocal); 
+    Wii_local = 1/f4*sel_mat*0.04.*exp(-0.038*diffAngleLocal); 
     Wii_local(isnan(Wii_local)) = 0; 
 
     % Weight Wi by the asymmetric coefficients
